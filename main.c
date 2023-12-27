531b459d84f6886b1b2a8c22bbfcfae3b9c3afb8
mcipolla <matcip@hotmail.com>
2023-12-27 19:51:57 +0100

    at()

--- main.c
+++ main.c
@@ -3,14 +3,10 @@
 
 int main()
 {
-	int i = 0;
-	t_string *s = String("prova");
-	printf("%s\n", s->begin());
-	printf("%s\n", s->data);
-	printf("%s\n", s->current);
 	t_string *s2 = String("seconda prova");
 	printf("%s\n", s2->begin());
 	printf("%s\n", s2->data);
 	printf("%s\n", s2->current);
 	printf("%s\n", s2->end() - 1);
+	printf("%s\n", s2->at(4));
 }
